0884d7d2f6eda8e59dca06c139c94a21ca765129
Alexis0816 <gilver.raza@utec.edu.pe>
2022-03-29 00:02:59 -0500

    Alexis Raza

--- circulararray.h
+++ circulararray.h
@@ -13,17 +13,26 @@ public:
     CircularArray();
     CircularArray(int _capacity);
     virtual ~CircularArray();
+
+
     void push_front(T data);
     void push_back(T data);
+
     void insert(T data, int pos);
+
     T pop_front();
     T pop_back();
     bool is_full();
     bool is_empty();
+
     int size();
     void clear();
+
+
     T &operator[](int);
     void sort();
+
+
     bool is_sorted();
     void reverse();
     string to_string(string sep=" ");
--- stack.h
+++ stack.h
@@ -1,6 +1,61 @@
 #include "circulararray.h"
 
+void resize(int* &array,int &longitud, int newlongitud){
+    int* arr_aux=new int[newlongitud];
+    for (int i = 0; i <longitud ; ++i) {
+        arr_aux[i]=array[i];
+    }
+    longitud=newlongitud;
+    array=arr_aux;
+}
+
+
 template <typename T>
-class StackArray : public CircularArray<T> { 
-    
+//Estructura LIFO (Last-In, First-Out)
+class StackArray:public CircularArray<T>{
+private:
+    T* array;
+    int capacity; //valor cambiable
+    int size;
+
+public:
+    StackArray(int max){//capacity=max;
+        size=0;
+        capacity=max;
+        array=new T[capacity];
+    };
+
+
+    void push_front(T data){
+        if(size == capacity){
+            resize(array,capacity,capacity*2); //O(n)
+        }
+        array[size]=data;//O(1)
+        size+=1;
+    };
+    int pop_back(){
+        int data=array[size-1];
+        size--;
+        return data;
+    };//O(1)
+    int top(){
+        int indexTop=size-1;
+        return array[indexTop];
+    };
+
+
+    int _size(){
+        return size;
+    }
+
+    void display(){
+        for (int i = 0; i <size ; ++i) {
+            cout<<array[i]<<" ";
+        }cout<<endl;
+    };
+
+    ~StackArray(){
+        delete [] array;
+    }
 };
+
