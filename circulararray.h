23eb75529894ef011e31df2a130d50b9e1cf8716
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2022-03-28 23:58:30 -0500

    Carlos Flores

--- circulararray.h
+++ circulararray.h
@@ -4,7 +4,7 @@ using namespace std;
 template <class T>
 class CircularArray
 {
-private:
+protected:
     T *array;
     int capacity;
     int back, front;
@@ -13,21 +13,20 @@ public:
     CircularArray();
     CircularArray(int _capacity);
     virtual ~CircularArray();
-    void push_front(T data);
-    void push_back(T data);
-    void insert(T data, int pos);
-    T pop_front();
-    T pop_back();
-    bool is_full();
-    bool is_empty();
-    int size();
-    void clear();
-    T &operator[](int);
-    void sort();
-    bool is_sorted();
-    void reverse();
+    void push_front(T data); // check
+    void push_back(T data); // check
+    void insert(T data, int pos); //
+    T pop_front(); //
+    T pop_back(); //
+    bool is_full(); // check
+    bool is_empty(); // check
+    int size(); 
+    void clear(); 
+    T &operator[](int); 
+    void sort(); // check
+    bool is_sorted(); // check
+    void reverse(); // check
     string to_string(string sep=" ");
-
 private:
     int next(int);
     int prev(int);
@@ -73,3 +72,67 @@ string CircularArray<T>::to_string(string sep)
         result += std::to_string((*this)[i]) + sep;
     return result;    
 }
+
+template <class T>
+bool CircularArray<T>::is_empty(){
+    if (front == -1 and back==-1) return true;
+    else return false;
+}
+
+template <class T>
+bool CircularArray<T>::is_full(){
+    if (back = capacity-1) return true;
+    else return false;
+}
+
+template <class T>
+void CircularArray<T>::push_front(T data){
+    for (int i = capacity; i > 0 ; i--) array[i] = array[i-1];
+    array[0] = data;
+    back++;
+}
+
+template <class T>
+void CircularArray<T>::push_back(T data){
+    if ( front == -1) front = 0;
+    back++;
+    array[back]= data;
+}
+
+template <class T>
+void CircularArray<T>::sort(){
+    int aux;
+    for(int i = 0; i < capacity; i++){
+        for(int x = 0; x < capacity; x++){
+            if(array[i] < array[x]){
+                aux = array[i];
+                array[i] = array[x];
+                array[x] = aux;
+            }
+        }
+    }
+}
+
+template <class T>
+bool CircularArray<T>::is_sorted(){
+    bool salida = false;
+    for (int i = 0; i < capacity-1; i++)
+    {
+        if (array[i+1] > array[i]) salida = true;
+        else salida = false;
+    }
+    return salida;
+}
+
+template <class T>
+void CircularArray<T>::reverse(){
+    int restador = 1;
+    for (int i = 0; i < capacity/2; i++){   
+        int aux=array[i];
+        array[i]= array[i+capacity-restador];
+        array[i+capacity-restador] = aux;
+        restador = restador+2;
+    }
+}
+
+
--- stack.h
+++ stack.h
@@ -2,5 +2,4 @@
 
 template <typename T>
 class StackArray : public CircularArray<T> { 
-    
 };
