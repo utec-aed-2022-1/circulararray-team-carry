e6325b9f7466b971953ec1e2534bdd7313465837
Alexis0816 <gilver.raza@utec.edu.pe>
2022-03-29 19:23:26 -0500

    Alexis Raza - QueueArray, StackArray

--- queue.h
+++ queue.h
@@ -1,28 +1,34 @@
 #include "circulararray.h"
-
+//Estructura LIFO (First-In, First-Out)
 template <typename T>
-class QueueArray : public CircularArray<T> { 
-    private:
-    T *array= getarray();
-    int size = getcapacity();
-    int front = getfront(); int back = getback();
+class QueueArray : public CircularArray<T> {
+private:
+    T *array;
+    int size ;
+    int eachSize;
+    int front ,  back ;
 
-    public:
+public:
     QueueArray(int _size);
-    ~QueueArray();
+
     bool isempty();
+    void resize();
     void enqueue(int _value);
     void dequeue();
     void showfront();
     void showback();
+    void Size();
     void displayQueue();
+    ~QueueArray();
 };
+
+
 
 template <class T>
 QueueArray<T>::QueueArray(int _size){
     CircularArray<T>::capacity = _size;
-
-    this->array = new int[_size];
+    this->eachSize=0;
+    this->array = new T[_size];
     this->size = _size;
     this->front = this->back = -1;
 }
@@ -31,48 +37,68 @@ template <class T>
 QueueArray<T>::~QueueArray(){delete[] array;}
 
 template <class T>
-bool QueueArray<T>::isempty(){
-    if (front ==-1 and back == -1)return true;
-    else return false;
+bool QueueArray<T>::isempty(){ bool verificar;
+    return (front ==-1 and back == -1) ?  verificar=true: verificar= false;
 }
+
+template<class T>
+void QueueArray<T>::resize(){
+    int* arr_aux=new T[size*2];
+    int i=0;
+    int j=front;
+    do {arr_aux[i++]=array[j];
+        j=(j+1)%size;
+    }while(j!=front);
+    front=0;
+    back=size-1;
+    array=arr_aux;
+    cout<<"Se ha redimensionado el QueueArray"<<endl;
+};
 
 template <class T>
 void QueueArray<T>::enqueue(int _value){
-    if (back == size-1) cout<<"Queue esta lleno\n";
-    else{
-        if (front == -1) front = 0;
+    if (back == size-1) {
+        cout<<"QueueArray esta lleno\n";
+        resize();
+    }
+    if (front == -1) front = 0;
         back++;
         array[back] = _value;
-    }
+        eachSize++;
 }
 
 template <class T>
 void QueueArray<T>::dequeue(){
-    if (isempty()) cout<<"Queue esta vacio\n";
-    else if (front == back){ front = back = -1;}
-    else front++;
+    if (isempty()) cout<<"QueueArray esta vacio\n";
+    else if (front == back){
+        front = back = -1;
+    }
+    else {front++ ;eachSize--;};
 }
 
 template <class T>
 void QueueArray<T>::showfront(){
-    if (isempty()) cout<<"Queue esta vacio\n";
-    else cout<<"el elemento front es: "<<array[front]<<endl;
-}
+    if (isempty()) cout<<"QueueArray esta vacio\n";
+    else cout<<"Front: "<<array[front]<<endl;}
 
 template <class T>
 void QueueArray<T>::showback(){
-    if (isempty()) cout<<"Queue esta vacio\n";
-    else cout<<"el elemento back es: "<<array[back]<<endl;
-}
+    if (isempty()) cout<<"QueueArray esta vacio\n";
+    else cout<<"Back: "<<array[back]<<endl;
+};
+
+template <class T>
+void QueueArray<T>::Size(){
+    cout<<"Tamanio QueueArray: "<<eachSize<<endl;
+};
 
 template <class T>
 void QueueArray<T>::displayQueue(){
-    if (isempty()) cout<<"Queue esta vacio\n";
-    else
-    {
+    if (isempty()) cout<<"QueueArray esta vacio\n";
+    else{
         for (int i = front; i <=back; i++){
             cout<<array[i]<<" ";
         }
         cout<<endl;
     }
-}
+};
